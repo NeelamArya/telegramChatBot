a14def4ed461df32a995faf8c666061c62f353d2
NeelamArya <neelamarya1218@gmail.com>
2026-01-15 00:28:00 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask
-import threading
 import asyncio
-from main import run_bot
+from flask import Flask, request
+from aiogram.types import Update
+
+from main import bot, process_update
 
 app = Flask(__name__)
 
@@ -10,12 +11,15 @@ app = Flask(__name__)
 def home():
     return "Bot is running!"
 
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    update = Update.model_validate(request.json)
+    asyncio.run(process_update(update))
+    return "ok"
+
 if __name__ == "__main__":
     print(">>> Flask started")
-
-    threading.Thread(
-        target=lambda: asyncio.run(run_bot()),
-        daemon=True
-    ).start()
-
-    app.run(host="0.0.0.0", port=10000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 10000))
+    )
