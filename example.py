b73243f8764a945be2c97a5ecbddd6742cea08b2
NeelamArya <neelamarya1218@gmail.com>
2026-01-15 00:40:32 +0530

    Update example.py

--- example.py
+++ example.py
@@ -2,7 +2,6 @@ import os
 import asyncio
 from flask import Flask, request
 from aiogram.types import Update
-
 from main import bot, process_update
 
 app = Flask(__name__)
@@ -14,12 +13,9 @@ def home():
 @app.route("/webhook", methods=["POST"])
 def webhook():
     update = Update.model_validate(request.json)
-    asyncio.run(process_update(update))
+    loop = asyncio.get_event_loop()
+    loop.create_task(process_update(update))
     return "ok"
 
 if __name__ == "__main__":
-    print(">>> Flask started")
-    app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 10000))
-    )
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
